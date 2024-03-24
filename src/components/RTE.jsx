54bdcd58da3c980da6f0b2e148c1078844f1ddd2
tusharawat01 <tushar0402tr@gmail.com>
2024-03-24 18:14:31 +0530

    Add something

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
+                        apiKey='uijzh0mflfinnwn2e7rs45rf158xisjzfthwvksp8a6bawv5'
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
