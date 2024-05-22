3f1f7f8bdcfa04cbdfce1e9f135e2615d73350e7
tusharawat01 <tushar0402tr@gmail.com>
2024-05-22 15:19:00 +0530

    Color and name changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }, [])
 
   return !loading ? (
-    <div className='min-h-screen flex  content-between bg-yellow-100'>
+    <div className='min-h-screen flex  content-between bg-green-100'>
       <div className='w-full block'>
         <Header />
         <main>
